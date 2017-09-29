be8afc9a5a7af49373e8684036e1e279ee0a5b9b
Stuart Mathews <stumathews@gmail.com>
2017-09-29 15:34:09 +0000

    init

---
+++ .svn/pristine/09/09561c8ff2fa3278dd8f4e53863ee5a9fb0fcace.svn-base
@@ -0,0 +1,18 @@
+//
+//  AppCompatServiceAppDelegate.h
+//  AppCompatService
+//
+//  Created by Stuart Mathews on 04/11/2010.
+//  Copyright Stuart Mathews Inc. 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppCompatServiceAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AppCompatService
+//
+//  Created by Stuart Mathews on 04/11/2010.
+//  Copyright Stuart Mathews Inc. 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
